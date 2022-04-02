d9d616306e327b5e3bbb8229730a9f011d9e80cf
Nigel Dias <nigeldias27@yahoo.com>
2022-04-02 23:07:29 +0530

    Created the Profile Book list page Refers to issues #6

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'bookitem.dart';
+import 'profile/profile_books.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Book(),
+      home: ProfileBooks(),
     );
   }
 }
