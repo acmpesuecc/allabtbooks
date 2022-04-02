82c5990215e54920b3daf4d733fb1be6062f9b5e
Nigel Dias <nigeldias27@yahoo.com>
2022-04-02 19:29:06 +0530

    Created the Profile Page and Book item page. Refers to issues #6 #5

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'profile/profile_books.dart';
+import 'bookitem.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ProfileBooks(),
+      home: Book(),
     );
   }
 }
