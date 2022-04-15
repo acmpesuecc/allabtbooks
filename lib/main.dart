ac409047cb88be697a26019574efa90e9dcaad9f
Lohithsurya <suryaln064@gmail.com>
2022-04-15 11:04:47 +0530

    fixed the orientation of the screen to up and down

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'bookitem.dart';
-
+import 'package:flutter/services.dart';
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+
+  ],);
   runApp(const MyApp());
 }
 
